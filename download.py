e98eff550669c118a00542bdff67caa43e42b750
XcelSurveying <gsurveyc@CNU0370G7X.lc.local>
2015-07-19 18:42:54 +0800

    updates

--- download.py
+++ download.py
@@ -12,13 +12,15 @@ def downloadVidList():
                 best = video.getbest()
                 best.download(quiet=False)
 	
-def VideoDescription(url):
+def VideoDescription():
+
+        url = Range('Sheet1', 'C3').value
         video = pafy.new(url)
         
         wb = Workbook.caller() # Creates a reference to the calling Excel file
 
         videoTitle = video.title
         
-        Range('Sheet1', 'C3').value = videoTitle
+        Range('Sheet1', 'G3').value = videoTitle
 
         
