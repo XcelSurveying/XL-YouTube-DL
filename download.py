612736dfa9f0757f5c2698eff54eada93cef0873
XcelSurveying <gsurveyc@CNU0370G7X.lc.local>
2015-07-19 15:49:24 +0800

    updates

---
+++ download.py
@@ -0,0 +1,24 @@
+from __future__ import print_function, unicode_literals
+import pafy
+import sys
+from xlwings import Workbook, Range
+
+def downloadVidList():
+        collection = ['Vsy1URDYK88']
+
+        for url in collection:
+                video = pafy.new(url)
+
+                best = video.getbest()
+                best.download(quiet=False)
+	
+def VideoDescription(url):
+        video = pafy.new(url)
+        
+        wb = Workbook.caller() # Creates a reference to the calling Excel file
+
+        videoTitle = video.title
+        
+        Range('Sheet1', 'C3').value = videoTitle
+
+        
